d5eddb2716bc744efea8c39f49e6a37f390f5fd0
Jeim05 <lopezvegajeimmy@gmail.com>
2024-12-03 19:57:58 -0600

    Configuración de rutas

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const config = require('./config')
 
+const clientes = require('./models/categorias/rutas')
+
 const app = express()
+
 
 app.set('port', config.app.port);
 
 // Rutas
-
+app.use('/api/categorias', clientes)
 
 
 module.exports = app;
