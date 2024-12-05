247fc9e59894cbe11761886c95b69fc16b5dab18
Jeim05 <lopezvegajeimmy@gmail.com>
2024-12-04 20:57:21 -0600

    Agregar cors para poder acceder desde el cliente

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const config = require('./config')
+const cors = require('cors')
 
 const clientes = require('./models/categorias/rutas')
 
 const app = express()
 
-
+// Configuracion del puerto
 app.set('port', config.app.port);
+
+// Configurar Cors
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 
 // Rutas
 app.use('/api/categorias', clientes)
