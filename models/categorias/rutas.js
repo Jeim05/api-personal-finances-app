c40f5ca66e29e3170be21191eb7ee5c74393abe0
Jeim05 <lopezvegajeimmy@gmail.com>
2024-12-05 21:55:33 -0600

    Update rutas.js
    Manejo de errores

--- models/categorias/rutas.js
+++ models/categorias/rutas.js
@@ -14,7 +14,7 @@ async function getCategories (req, res) {
         const items = await controller.getCategories()
         response.success(req, res, items, 200)
     } catch (error) {
-        response.error(req, res, error, 500)
+        next(err)
     }
 };
 
@@ -23,16 +23,16 @@ async function getCategories (req, res) {
         const items = await controller.getById(req.params.id)
         response.success(req, res, items, 200)
     } catch (error) {
-        response.error(req, res, error, 500)
+        next(err)
     }
 };
 
-async function deleteCategorie(req, res) {
+async function deleteCategorie(req, res, next) {
     try {
         const items = await controller.deleteCategorie(req.body)
         response.success(req, res, 'Item eliminado satisfactoriamente', 200)
     } catch (error) {
-        response.error(req, res, error, 500)
+        next(err)
     }
 };
 
