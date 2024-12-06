40b091a513456d0a445f34adb4876c92fbcdb52a
Jeim05 <lopezvegajeimmy@gmail.com>
2024-12-05 21:09:47 -0600

    Update rutas.js
    Refactorizacion de rutas

--- models/categorias/rutas.js
+++ models/categorias/rutas.js
@@ -5,16 +5,19 @@ const controller = require('./controlador')
 
 const router = express.Router();
 
-router.get('/', async function (req, res) {
+router.get('/', getCategories)
+router.get('/:id', getById) 
+
+async function getCategories (req, res) {
     try {
         const items = await controller.getCategories()
         response.success(req, res, items, 200)
     } catch (error) {
         response.error(req, res, error, 500)
     }
-});
+};
 
-router.get('/:id', async function (req, res) {
+ async function getById(req, res) {
     try {
         const items = await controller.getById(req.params.id)
         response.success(req, res, items, 200)
@@ -22,7 +25,7 @@ router.get('/:id', async function (req, res) {
         response.error(req, res, error, 500)
     }
 
-});
+};
 
 module.exports = router
 
