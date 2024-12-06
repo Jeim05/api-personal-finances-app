9f946df909ea4289578ad3876dcd844a27640e02
Jeim05 <lopezvegajeimmy@gmail.com>
2024-12-05 21:52:25 -0600

    Configuracion de archivos para errores

---
+++ red/errors.js
@@ -0,0 +1,12 @@
+const response = require('./respuestas');
+
+function error(err, req, res, next){
+    console.log('[error', err)
+
+    const message = err.message || 'Error Interno'
+    const status = err.statusCode || 500;
+
+    respuesta.error(req, res, message, status)
+}
+
+module.exports = error
