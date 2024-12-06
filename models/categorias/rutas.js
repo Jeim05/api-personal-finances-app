46452ed1aecd169fff577b0fdeb21a8e693fa77e
Jeim05 <lopezvegajeimmy@gmail.com>
2024-12-05 20:46:09 -0600

    Instalación de morgan

--- models/categorias/rutas.js
+++ models/categorias/rutas.js
@@ -1,15 +1,13 @@
-const express =  require('express');
+const express = require('express');
 
 const response = require('../../red/respuestas')
 const controller = require('./controlador')
 
 const router = express.Router();
 
-router.get('/', function(req, res){
-    const getCategories = controller.getCategories()
-    .then((items)=>{
-        response.success(req, res, items, 200)   
-    })
+router.get('/', async function (req, res) {
+    const items = await controller.getCategories()
+    response.success(req, res, items, 200)
 });
 
 module.exports = router
